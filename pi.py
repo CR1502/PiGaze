a78b00d9cb20a1e286c884f623b2f4be204485c2
Justin Galin <108830719+Airport237@users.noreply.github.com>
2025-02-05 19:44:26 -0500

    Refactor

--- pi.py
+++ pi.py
@@ -72,32 +72,30 @@ trained_model = load_model('pigaze_model.pth')
 
 
 while True:
-	while True:
-		frame = picam2.capture_array()
+	frame = picam2.capture_array()
 
-		frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-		# Extract image from frame
-		image = cv2.imwrite('image.jpg', frame_rgb)
+	# Extract image from frame
+	image = cv2.imwrite('image.jpg', frame_rgb)
 
-		# Make Prediction
-		gaze_target = predict_gaze(trained_model, 'image.jpg', transform)
+	# Make Prediction
+	gaze_target = predict_gaze(trained_model, 'image.jpg', transform)
 
-		# Print Model Predictions
-		print(f"Gaze Target {gaze_target}")
+	# Print Model Predictions
+	print(f"Gaze Target {gaze_target}")
 
-		# Display Gaze Coordinates on frame
-		x, y, = gaze_target[0]
-		x = int(x)
-		y = int(y)
-		cv2.circle(frame_rgb, (x, y), 3, (255, 0, 0), 5)
-		cv2.imshow("Frame", frame_rgb)
+	# Display Gaze Coordinates on frame
+	x, y, = gaze_target[0]
+	x = int(x)
+	y = int(y)
+	cv2.circle(frame_rgb, (x, y), 3, (255, 0, 0), 5)
+	cv2.imshow("Frame", frame_rgb)
 
-		k = cv2.waitKey(1)
-		if (k%256 == 27):
-			break
+	k = cv2.waitKey(1)
+	if (k%256 == 27):
+		break
 
-	break
 picam2.stop()
 #out.release()
 cv2.destroyAllWindows()
